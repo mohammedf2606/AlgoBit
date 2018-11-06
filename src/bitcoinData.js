144d762fab44c05044020bd40116d2237e541bf0
Amin <moradi.amin74@gmail.com>
2018-11-06 20:47:33 +0000

    data change

--- src/bitcoinData.js
+++ src/bitcoinData.js
@@ -32,18 +32,55 @@ const generateChartData = (labels, data)=>{
 }
 
 
-
-/*
-csv().fromString(data.data).then((jsonObj)=>{
-
-})
-
-*/
-
-
-const fetchBitcoinData = function(){
-
+function generateTime(data){
+  // typeof data = array
+  const times = [];
+  for(let i = 0; i < data.length; i++){
+    times.push(data[i].Time);
+  }
+  return times;// some data
 }
+
+function generateBitstamp(data){
+  // typeof data = array
+  const bitstampData = [];
+  for(let i = 0; i < data.length; i++){
+    bitstampData.push(data[i].bitstamp);
+  }
+  return bitstampData;// some data
+}
+
+
+
+// const fetchBitcoinData = function(){
+//   return new Promise(function(resolve, reject){
+//     axios.get(smallData).then(function(axiosFirstData){
+//       console.log({axiosFirstData});
+//       csv().fromString(axiosFirstData.data).then((csvToObj)=>{
+//
+//         const justTime = generateTime(csvToObj);
+//         const bitstampData = generateBitstamp(csvToObj);
+//         console.log({justTime});
+//         console.log({bitstampData});
+//       })
+//     })
+//   })
+// }
+
+
+const fetchBitcoinData = ()=> (new Promise((resolve, reject)=>{
+      axios.get(smallData)
+      .then((data)=>{
+        return csv().fromString(data.data)
+      })
+      .then((jsonObj)=>{
+          const allTimes = jsonObj.map((item)=> item.Time.toString());
+          const allPrices = jsonObj.map((item)=> item.bitstamp);
+          resolve(generateChartData(allTimes, allPrices))
+      }).catch((err)=>{
+        console.log('err',err);
+      })
+  }))
 
 
 export default fetchBitcoinData;
